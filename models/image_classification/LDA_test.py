110f3923054daa4ac366e9378e354303943992e3
Michele Alberti <albertimichele91@hotmail.com>
2020-02-20 16:17:59 +0100

    moved avg pool into last conv layer

--- models/image_classification/LDA_test.py
+++ models/image_classification/LDA_test.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+from torch import sigmoid
+
 from models.registry import Model
 import torch.nn.functional as F
 
@@ -9,63 +11,44 @@ class Flatten(nn.Module):
 
 class Swish(nn.Module):
     def forward(self, x):
-        return x * F.sigmoid(x)
+        return x * sigmoid(x)
 
 #######################################################################################################################
 #######################################################################################################################
 #######################################################################################################################
-class LDA_CIFAR(nn.Module):
-    """
-    :var conv1   : torch.nn.Conv2d
-    :var conv2   : torch.nn.Conv2d
-    :var conv3   : torch.nn.Conv2d
-        The first three convolutional layers of the network
+@Model
+class InitBaseline(nn.Module):
+    expected_input_size = (149, 149)
 
-    :var fc      : torch.nn.Linear
-        Final fully connected layer
-    """
+    def __init__(self, num_classes, **kwargs):
+        super(InitBaseline, self).__init__()
 
-    def __init__(self, num_classes, input_channels=3, **kwargs):
-        """
-        :param num_classes: the number of classes in the dataset
-        """
-        super(LDA_CIFAR, self).__init__()
-
-        self.expected_input_size = (32, 32)
-
-        num_filters_conv1 = 150
-        num_filters_conv2 = 150
+        ocl1 = 32
 
         # First layer
         self.conv1 = nn.Sequential(
-            nn.Conv2d(input_channels, num_filters_conv1, kernel_size=5, stride=3),
-            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=3, out_channels=ocl1, kernel_size=8, stride=3, padding=0),
+            Swish(),
         )
         # Second layer
         self.conv2 = nn.Sequential(
-            nn.Conv2d(num_filters_conv1, num_filters_conv2, kernel_size=3, stride=2),
-            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=ocl1, out_channels= ocl1 * 2,  kernel_size=5, stride=3, padding=1),
+            Swish(),
         )
         # Third layer
         self.conv3 = nn.Sequential(
-            nn.Conv2d(num_filters_conv2, 72, kernel_size=3, stride=1),
-            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=ocl1 * 2, out_channels=ocl1 * 4,  kernel_size=3, stride=1, padding=1),
+            Swish(),
+            nn.AvgPool2d(kernel_size=16, stride=1),
+            Flatten(),
         )
 
         # Classification layer
         self.fc = nn.Sequential(
-            Flatten(),
-            nn.Linear(288, num_classes)
+            nn.Linear(ocl1 * 4, num_classes)
         )
 
     def forward(self, x):
-        """
-        Computes forward pass on the network
-        :param x: torch.Tensor
-            The input to the model
-        :return: torch.Tensor
-            Activations of the fully connected layer
-        """
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
@@ -89,8 +72,8 @@ class LDA_Simple(nn.Module):
         self.conv1 = nn.Sequential(  # in: 32x32x3 out: 8 x 8 x layer_1_neurons
             nn.Conv2d(in_channels=3, out_channels=layer_1_neurons, kernel_size=4, stride=4,
                       padding=0),
-            #Swish()
-            nn.Softsign()
+            Swish()
+            # nn.Softsign()
         )
         # Classification layer
         self.cl = nn.Sequential(
