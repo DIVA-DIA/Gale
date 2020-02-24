e421ea49d1ed7567b7071f2df4afdebd7d320c2b
Michele Alberti <albertimichele91@hotmail.com>
2020-02-24 21:40:18 +0100

    added extra CL arguments for new LDA methods

--- template/runner/image_classification/CL_arguments.py
+++ template/runner/image_classification/CL_arguments.py
@@ -1,3 +1,5 @@
+import argparse
+
 from init import advanced_init
 from template.runner.base import BaseCLArguments
 
@@ -18,6 +20,16 @@ class CLArguments(BaseCLArguments):
 
         return args, self.parser
 
+    def str2bool(self, v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
     def _init_options(self):
         """
         Options specific for network initialization methods
@@ -36,9 +48,37 @@ class CLArguments(BaseCLArguments):
                                  help='number of samples to use to perform data-driven initialization')
         parser_init.add_argument('--init-function',
                                  choices=init_options,
-                                 default="pure_lda",
                                  help='which initialization function should be used.')
         parser_init.add_argument('--max-patches',
                                  type=float,
-                                 default=0.9,
+                                 default=0.99,
                                  help='ratio of patch to extract from each sample for conv layers')
+
+        parser_init.add_argument("--conv-normalize",
+                                 type=self.str2bool,
+                                 help="Flag for normalizing conv weights")
+        parser_init.add_argument("--conv-standardize",
+                                 type=self.str2bool,
+                                 default="True",
+                                 help="Flag for standardizing conv weights")
+        parser_init.add_argument("--conv-scale",
+                                 type=self.str2bool,
+                                 help="Flag for scaling conv weights")
+
+        parser_init.add_argument("--lin-normalize",
+                                 type=self.str2bool,
+                                 default="True",
+                                 help="Flag for normalizing linear weights")
+        parser_init.add_argument("--lin-standardize",
+                                 type=self.str2bool,
+                                 default="True",
+                                 help="Flag for standardizing linear weights")
+        parser_init.add_argument("--lin-scale",
+                                 type=self.str2bool,
+                                 default="True",
+                                 help="Flag for normalizing linear weights")
+
+        parser_init.add_argument('--trim-lda-iterations',
+                                 type=int,
+                                 default=0,
+                                 help='number of iterations for trimming the points of the lda')
