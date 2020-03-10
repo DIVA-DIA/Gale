fixed a bug on scaling weights for the random case

init/advanced_init.py
@@ -52,8 +52,8 @@ def _standardize_weights(w, b):
         B and W standard normalized
     """
     logging.debug(f"w={w.shape} b={b.shape}")
-    # The flattening is used for the random init which has the W matrix still in conv-layer shape
-    w_tmp = w if len(w.shape) == 2 else _flatten_conv_filters(w)
+    # The flattening&transposition is used for the random init which has the W matrix still in conv-layer shape
+    w_tmp = w if len(w.shape) == 2 else _flatten_conv_filters(w).T
     joint_matrices = np.concatenate((w_tmp, np.expand_dims(b, axis=1)), axis=1)
     mean = np.mean(joint_matrices)
     std = np.std(joint_matrices)