b72f27ce662b92bc48cf19502dad3554e9ede76f
Lars Voegtlin <lars.voegtlin@unifr.ch>
2020-10-26 11:17:08 +0100

    removed the extra init stuff from the environment from scratch

--- create_environment_from_scratch.sh
+++ create_environment_from_scratch.sh
@@ -4,6 +4,3 @@ conda create -y -n gale python=3.7.4 colorlog numpy pandas tqdm matplotlib seabo
 source activate gale
 pip install sigopt tensorboardx darwin-py wandb psutil
 
-# Extra stuff for maphd branch
-pip install git+https://github.com/NarayanSchuetz/SpectralLayersPyTorch
-conda install scikit-learn
